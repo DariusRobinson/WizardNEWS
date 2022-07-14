8878f62b73cb062afb330495ccf0c7b986661790
bcova <brandoncova@gmail.com>
2022-07-14 12:26:25 -0500

    First push

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const morgan = require("morgan")
+const postBank = require("./postBank")
+
+
 const app = express();
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
 const PORT = 1337;
 
+
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+
+const posts = postBank.list()
+
+
+const html = `<!DOCTYPE html>
+<html>
+<head>
+    <title>Wizard News</title>
+</head>
+<body>
+    <ul>
+       ${posts.map(post => `<li>${post.title} - ${post.name}</li>`).join('')}
+    </ul>
+</body>
+</html>`
+
+
+res.send(html)
+
+})
+
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+    console.log(`App listening in port ${PORT}`);
+  });
