8b2eb562aa88d68d9e22ae8c534e54be56ab17cf
Darius Robinson <robinson.darius090@gmail.com>
2022-07-14 14:20:45 -0400

    finished the single post display

--- app.js
+++ app.js
@@ -1,36 +1,81 @@
 const express = require("express");
-const morgan = require("morgan")
-const postBank = require("./postBank")
-
+const morgan = require("morgan");
+const postBank = require("./postBank");
 
 const app = express();
 const PORT = 1337;
 
-
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+  const posts = postBank.list();
 
-const posts = postBank.list()
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res)=> { 
+const id = req.params.id;
+const post = postBank.find(id);
 
 
 const html = `<!DOCTYPE html>
-<html>
-<head>
+  <html>
+  <head>
     <title>Wizard News</title>
-</head>
-<body>
-    <ul>
-       ${posts.map(post => `<li>${post.title} - ${post.name}</li>`).join('')}
-    </ul>
-</body>
-</html>`
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class='news-item'>
+      <p>
+        <span class="news-position">${post.id}. ▲</span>${post.title}
+        <small>(by ${post.name})</small>
+      </p>
+      <p class="news-info">
+        ${post.content}  | ${post.date}
+      </p>
+    </div>
+  </body>
+</html>`;
+
 
 
 res.send(html)
 
+
 })
 
+
+
 app.listen(PORT, () => {
-    console.log(`App listening in port ${PORT}`);
-  });
+  console.log(`App listening in port ${PORT}`);
+});
